4c220cdea9e96551715ba7c2e8e6a85037364dfd
sankar <sankarsuresh1997@gmail.com>
2022-11-21 10:22:40 +0530

    calculator

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        System.out.println("welcome to calculator");
+        System.out.println("please enter two numbers");
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("enter your choice");
+        int n=sc.nextInt();
+
+    }
+}
